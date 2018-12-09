c484685d2b9b93eb45b7200dd45089b13b47e189
Тимур Абдулов <hostingurifa@gmail.com>
2018-12-10 03:31:24 +0500

    Добавлено Сообщение о завершении работы программы

--- src/main.c
+++ src/main.c
@@ -65,6 +65,8 @@ char *basename(char *path)
 
 int main(int argc, char **argv)
 {
+
+
 	char *source = NULL;
 	char buf[255];
 	char *expression = source;
@@ -94,5 +96,8 @@ int main(int argc, char **argv)
 				break;
 		}
 	}
+
+    printf("End.\n");
+
 	return 0;
 }
