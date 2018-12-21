634aae4ba675d7a7bdfc93e065ce85e38d338c80
fromasmtodiasm <chiappa24@gmail.com>
2018-12-21 11:12:33 +0300

    Processing command line args

--- src/main.c
+++ src/main.c
@@ -1,31 +1,61 @@
+/***************************************************************************/
+/****************************** Includes ***********************************/
+/***************************************************************************/
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "fileutils.h"
 #include "interpreter.h"
 #include "preprocessor.h"
 #include "debug.h"
 
+/***************************************************************************/
+/***************** Defines used by this module only ************************/
+/***************************************************************************/
 #if !defined _MSC_VER
 #define getline mygetline
 #endif
 
 #define DEBUGING_LEVEL DEBUG_TRACE
 
+/***************************************************************************/
+/************************* Global Variables ********************************/
+/***************************************************************************/
+struct globalArgs_t {
+    int noIndex;                /* параметр -I */
+    char *langCode;             /* параметр -l */
+    const char *outFileName;    /* параметр -o */
+    FILE *outFile;
+    int verbosity;              /* параметр -v */
+    char **inputFiles;          /* входные файлы */
+    int numInputFiles;          /* число входных файлов */
+} globalArgs;
+
+/***************************************************************************/
+/********** Global Variables defined for this module only ******************/
+/************************ (should be static) *******************************/
+/***************************************************************************/
+static const char *optString = "Il:o:vh?";
+
+int process_args(int argc, char **argv) {
+  int res;
+
+  while (getopt(argc, argv, optString) != -1)
+  {
+    printf("Here\n"); 
+  }
+
+  return res;
+}
+
 int getline(FILE *fp) {
   int ch = EOF;
   int len = 0;
   int lines = 0;
-  while ((ch = getc(fp)) != EOF) {
-    if (ch != '\n') {
-      len++;
-    } else {
-      len++;
-      break;
-    }
-  }
+  
   return len;
 }
 
@@ -59,9 +89,19 @@ int main(int argc, char **argv) {
   char buf[255];
   char *expression = source;
   FILE *test;
+  int opt = 0;
+
+  globalArgs.noIndex = 0;     /* false */
+  globalArgs.langCode = NULL;
+  globalArgs.outFileName = NULL;
+  globalArgs.outFile = NULL;
+  globalArgs.verbosity = 0;
+  globalArgs.inputFiles = NULL;
+  globalArgs.numInputFiles = 0;
 
   SET_DEBUG_LVL(DEBUGING_LEVEL);
   
+  process_args(argc, argv);
   if (argc > 1) {
     expression = buf;
     int cur_file = 1;
